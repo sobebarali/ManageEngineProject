066cc82cb09b1d3010901bc7a7cb3208b597413d
Manu <mangalmanu28@gmail.com>
2022-08-08 12:01:07 +0530

    done

--- log.js
+++ log.js
@@ -58,7 +58,7 @@ if(filterarray.length==0){
     document.getElementById("submit").style.marginBottom="30px"
 }else{
     localStorage.setItem("userInfo",JSON.stringify(filterarray[0]));
-    window.location.href="";
+    window.location.href="http://127.0.0.1:5501/ManageEngineProject/pages/index.html";
 
 }
 
