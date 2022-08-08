57d57e94672988f08de02d2b0d09abac2275c041
Jadhao2500 <saurabhjadhao001@gmail.com>
2022-08-08 10:04:23 +0530

    productDataAndJson

---
+++ productData/components/navbar.js
@@ -0,0 +1,5 @@
+function navbar(){
+    return'<div id="navbar"><div class="navImg"><a href="index.html"><img src="https://www.manageengine.com/images/logo/manageengine-logo.svg" alt="navImg1"></a></div><div class="options"><h3><a href="#">Products</a></h3><h3><a href="#">Solutions</a></h3><h3><a href="#">Company</a></h3><h3><a href="#">Events</a></h3><h3><a href="#">Login</a></h3><h3><a href="#">Register</a></h3></div></div>'
+}
+
+export {navbar}
---
+++ productData/pages/product.js
@@ -0,0 +1,55 @@
+let tbody=document.getElementById("tbody");
+
+const getdata=async(url)=>{
+    try {
+        let res=await fetch(url)
+        let data=await res.json();
+        return data;
+    } catch (error) {
+        console.log(error)
+    }
+}
+let checked=[];
+const displayData=(data,tbody)=>{
+    
+    data.forEach((ele)=>{
+        let tr=document.createElement("tr");
+
+        let td1=document.createElement("td");
+        td1.textContent=ele.productDetail;
+        td1.style.color="#444444"
+        let td2=document.createElement("td")
+        td2.textContent=`US$${ele.price}`
+        td2.style.color="#A52A2A"
+        let td3=document.createElement("td")
+        td3.textContent="included"
+        td3.style.color="#A52A2A"
+        let td4=document.createElement("input");
+        td4.type="checkbox"
+        td4.setAttribute("id",`${ele.id}`);
+        checked.push(`${ele.id}`);
+        tr.append(td4,td1,td2,td3)
+        tbody.append(tr);
+    })
+    // console.log(checked)
+   
+}
+document.getElementById("cart").addEventListener("click",displaycheck)
+function displaycheck(){
+    let filterarr=checked.filter(function(elem){
+        return document.getElementById(elem).checked==true;
+    })
+    console.log(filterarr)
+    localStorage.setItem("checked_products",JSON.stringify(filterarr))
+}
+
+
+const initFunc= async()=>{
+    try {
+        let data=await getdata("http://localhost:3000/data")
+        displayData(data,tbody)
+    } catch (error) {
+        console.log(error)
+    }
+}
+initFunc();
